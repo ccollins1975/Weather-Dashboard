2612f709d62c3365f076f35690725a1a408415b8
Chris Collins <ccollins36@icloud.com>
2020-09-20 20:25:53 -0400

    adding days and data

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
 
-
+var searchHistory = JSON.parse(window.localStorage.getItem("history"))|| [];
 
     var apikey = "6e4f6ce6e647755b62c7319704cf675f";
-    // api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
-    // units=metric
+   
     $("#search-button").on("click", function () {
         console.log("click");
         var city = $("#city").val().trim();
         console.log(city)
         searchCity(city);
+
 
 
     });
@@ -22,21 +22,79 @@ $(document).ready(function () {
             dataType: "json",
             success: function (data) {
                 console.log(data)
-                // callback for uv index and 5 day forcast
-                uvindex(lat,lon);
-                forcast(city);
+                // callback for uv index and 5 day forcas 
+                var name = $("<h3>").text(data.name)
+                var temp =$("<p>").text("Current Temperature: " + data.main.temp);
+                var humidity =$("<p>").text("Current Humidity: " + data.main.humidity);
+                var windSpeed =$("<p>").text("Wind Speed: " + data.wind.speed);
+
+
+
+                $("#current").append(name, temp,humidity,windSpeed);
+                uvIndex(data.coord.lat,data.coord.lon);
+                forecast(city);
+
+                
 
             
             }
         })
     }
-function uvindex(lat,lon){
-    // ajax call for uv index
-}
-function forcast(city){
-    // ajax call 5 day forcast
+function uvIndex(lat,lon){
+
+    var query =`https://api.openweathermap.org/data/2.5/uvi?appid=${apikey}&lat=${lat}&lon=${lon}`;
+    $.ajax({
+        type: "GET",
+        url: query,
+        dataType: "json",
+        success: function (data) {
+            console.log(data)
+            // callback for uv index and 5 day forecast
+           var uv = $("<p>").text("UV Index: " + data.value);
+        $("#current").append(uv);
+        
+        }
+})
 
     
+}
+function forecast(city){
+
+    
+    var query =`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}&units=imperial`;
+    $.ajax({
+        type: "GET",
+        url: query,
+        dataType: "json",
+        success: function (data) {
+            console.log(data)
+            // callback for uv index and 5 day forcast
+            var days = data.list.filter((day)=>{
+                return day.dt_txt.includes("15:00:00")
+            });
+           
+            days.forEach((day)=>{
+                var card = $("<div>").addClass("card col-sm-2");
+                var body =$("<div>").addClass("body");
+                var date =$("<h5>").text(new Date(day.dt_txt).toLocaleDateString());
+                var name = $("<h3>").text(day.name)
+                var temp =$("<p>").text("Current Temperature: " + day.main.temp);
+                var humidity =$("<p>").text("Current Humidity: " + day.main.humidity);
+                var windSpeed =$("<p>").text("Wind Speed: " + day.wind.speed);
+                    
+                card.append(date,name,temp,humidity,windSpeed);
+                body.append(date);
+                $("#forecast").append(card)
+            });
+            
+
+
+        
+        }
+    })
+   
+
+
 }
 
 });
